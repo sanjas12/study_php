cb1281ff371829904ff0210509f9b16c31ee7f35
z500 <sanja100v@mail.ru>
2021-06-01 08:35:42 +0300

    Update

--- chapter_4_basic.php
+++ chapter_4_basic.php
@@ -1,3 +1,9 @@
 <?php // chapter_3_basic_php.php
     $month = "Март";
-    if ($month == 'Март') echo 'Пришла весна';
+    if ($month == 'Март') echo 'Пришла весна'. "\n";
+
+    $a = '1000';
+    $b = '+1000';
+
+    if ($a == $b) echo 1,"\n";
+    if ($a === $b) echo 2;
